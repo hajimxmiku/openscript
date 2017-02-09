6749afb9096d41add253d3824a6484dac797ee36
hajimxmiku <anakhandsomee@gmail.com>
2017-02-09 20:54:03 +0800

    Add files via upload

---
+++ ssh-installer.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# go to root
+cd
+
+# THIS SCRIPT HAS BEEN MADED BY HAJIMXMIKU IF YOU WANT TO EDIT PLEASE ASK A PERMISSION FIRST,YOU ARE FREE TO USE THIS SCRIPT
+
+# disable ipv6
+echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6
+sed -i '$ i\echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6' /etc/rc.local
+
+# install wget and curl
+apt-get update
+apt-get -y install wget curl
+
+# Change to Time GMT+8
+ln -fs /usr/share/zoneinfo/Asia/Kuala_Lumpur /etc/localtime
+
+# set locale
+sed -i 's/AcceptEnv/#AcceptEnv/g' /etc/ssh/sshd_config
+service ssh restart
+
+# remove unused
+apt-get -y --purge remove samba*;
+apt-get -y --purge remove apache2*;
+apt-get -y --purge remove sendmail*;
+apt-get -y --purge remove bind9*;
+
+# update
+apt-get update 
+apt-get -y upgrade
+
+# setting port ssh
+sed -i '/Port 22/a Port 143' /etc/ssh/sshd_config
+sed -i 's/Port 22/Port  22/g' /etc/ssh/sshd_config
+service ssh restart
+
+# install dropbear
+apt-get -y install dropbear
+sed -i 's/NO_START=1/NO_START=0/g' /etc/default/dropbear
+sed -i 's/DROPBEAR_PORT=22/DROPBEAR_PORT=443/g' /etc/default/dropbear
+sed -i 's/DROPBEAR_EXTRA_ARGS=/DROPBEAR_EXTRA_ARGS="-p 109 -p 110"/g' /etc/default/dropbear
+echo "/bin/false" >> /etc/shells
+service ssh restart
+service dropbear restart
+
+#info
+clear
+echo -e "\e[32m auto script by: Hajimxmiku"
+echo "OpenSSH  : 22, 143"
+echo "Dropbear : 109, 110, 443"
+
